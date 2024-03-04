0712c07584f130d3cee2686c45deb423966a9cf9
rajkumar <rajkumarrajkumar061@gmail.com>
2024-03-05 00:29:36 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,30 @@
+"use client";
+import AboutSectionOne from "@/components/About/AboutSectionOne";
+import AboutSectionTwo from "@/components/About/AboutSectionTwo";
+import Brands from "@/components/Brands";
+import ScrollUp from "@/components/Common/ScrollUp";
+import Contact from "@/components/Contact";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Video from "@/components/Video";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "MSquare | partner for comprehensive data solutions and information management",
+  description: " We specialize in a suite of services designed to streamline your data processes, enhance accuracy, and fortify your information security",
+  // other metadata
+};
+
+export default function Home() {
+  return (
+    <>
+      <ScrollUp />
+      <Hero />
+      <Features />
+      <Video />
+      <AboutSectionOne />
+      <AboutSectionTwo />
+      <Contact />
+    </>
+  );
+}
---
+++ src/components/Contact/index.tsx
@@ -0,0 +1,108 @@
+import SectionTitle from "../Common/SectionTitle";
+import NewsLatterBox from "./NewsLatterBox";
+
+const Contact = () => {
+  return (
+    <section id="contact" className="overflow-hidden py-16 md:py-20 lg:py-28">
+       <SectionTitle
+          title=""
+          paragraph=""
+          center
+        />
+          <div
+        className={`w-full  mx-auto text-center `}
+        style={{ maxWidth: 770, marginBottom: 100 }}
+      >
+        <h2 className="mb-4 text-3xl font-bold !leading-tight text-black dark:text-white sm:text-4xl md:text-[45px]">
+        Contact Us
+        </h2>
+        <p className="text-base !leading-relaxed text-body-color md:text-lg">
+        Ready to elevate your data management and security? Get in touch with MSquare. Our experts are here to answer your inquiries, discuss your specific needs, and chart a course towards more efficient, secure, and streamlined data processes. You + us = awesome.
+        </p>
+      </div>
+      <div className="container">
+        <div className="-mx-4 flex flex-wrap">
+          <div className="w-full px-4 lg:w-7/12 xl:w-8/12">
+            <div
+              className="mb-12 rounded-sm bg-white px-8 py-11 shadow-three dark:bg-gray-dark sm:p-[55px] lg:mb-5 lg:px-8 xl:p-[55px]"
+              data-wow-delay=".15s
+              "
+            >
+              <h2 className="mb-3 text-2xl font-bold text-black dark:text-white sm:text-3xl lg:text-2xl xl:text-3xl">
+                Need Help? Open a Ticket
+              </h2>
+              <p className="mb-12 text-base font-medium text-body-color">
+                Our support team will get back to you ASAP via email.
+              </p>
+              <form>
+                <div className="-mx-4 flex flex-wrap">
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="name"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Name
+                      </label>
+                      <input
+                        type="text"
+                        placeholder="Enter your name"
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4 md:w-1/2">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="email"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Email
+                      </label>
+                      <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="border-stroke w-full rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      />
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <div className="mb-8">
+                      <label
+                        htmlFor="message"
+                        className="mb-3 block text-sm font-medium text-dark dark:text-white"
+                      >
+                        Your Message
+                      </label>
+                      <textarea
+                        name="message"
+                        rows={5}
+                        placeholder="Enter your Message"
+                        className="border-stroke w-full resize-none rounded-sm border bg-[#f8f8f8] px-6 py-3 text-base text-body-color outline-none focus:border-primary dark:border-transparent dark:bg-[#2C303B] dark:text-body-color-dark dark:shadow-two dark:focus:border-primary dark:focus:shadow-none"
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div className="w-full px-4">
+                    <button className="rounded-sm bg-primary px-9 py-4 text-base font-medium text-white shadow-submit duration-300 hover:bg-primary/90 dark:shadow-submit-dark">
+                      Submit Ticket
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="w-full px-4 lg:w-5/12 xl:w-4/12">
+            {/* <NewsLatterBox /> */}
+
+
+          <iframe className="w-full h-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62883.09595735833!2d78.12282365!3d9.91783685!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b00c582b1189633%3A0xdc955b7264f63933!2sMadurai%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1709384858769!5m2!1sen!2sin"  loading="lazy" referrerPolicy="no-referrer-when-downgrade" frameBorder="0" allowFullScreen></iframe>
+
+
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
