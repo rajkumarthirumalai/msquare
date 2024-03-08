431b04ee35d5925b290b8ccb1b58776c41fc8295
rajkumar <rajkumarrajkumar061@gmail.com>
2024-03-09 02:32:14 +0530

    image and content

--- src/components/About/AboutSectionOne.tsx
+++ src/components/About/AboutSectionOne.tsx
@@ -24,8 +24,8 @@ const AboutSectionOne = () => {
           <div className="-mx-4 flex flex-wrap items-center">
             <div className="w-full px-4 lg:w-1/2">
               <SectionTitle
-                title="Crafted for Startup, SaaS and Business Sites."
-                paragraph="The main ‘thrust’ is to focus on educating attendees on how to best protect highly vulnerable business applications with interactive panel discussions and roundtables."
+                title="Empowering Tech Startups with Cutting-Edge Solutions."
+                paragraph="Our focus is on equipping startups with innovative tools and strategies to navigate the dynamic tech landscape. We foster interactive learning environments to ensure optimal protection of business applications."
                 mb="44px"
               />
 
@@ -35,15 +35,14 @@ const AboutSectionOne = () => {
               >
                 <div className="mx-[-12px] flex flex-wrap">
                   <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Premium quality" />
-                    <List text="Tailwind CSS" />
-                    <List text="Use for lifetime" />
+                    <List text="Tailored solutions" />
+                    <List text="Cutting-edge tech" />
+                    <List text="Lifetime support" />
                   </div>
-
                   <div className="w-full px-3 sm:w-1/2 lg:w-full xl:w-1/2">
-                    <List text="Next.js" />
-                    <List text="Rich documentation" />
-                    <List text="Developer friendly" />
+                    <List text="Next-gen frameworks" />
+                    <List text="Comprehensive docs" />
+                    <List text="Developer-friendly" />
                   </div>
                 </div>
               </div>
@@ -52,13 +51,13 @@ const AboutSectionOne = () => {
             <div className="w-full px-4 lg:w-1/2">
               <div className="relative mx-auto aspect-[25/24] max-w-[500px] lg:mr-0">
                 <Image
-                  src="/images/about/about-image.svg"
+                  src="/images/about/Endpoint-amico.svg"
                   alt="about-image"
                   fill
                   className="mx-auto max-w-full drop-shadow-three dark:hidden dark:drop-shadow-none lg:mr-0"
                 />
                 <Image
-                  src="/images/about/about-image-dark.svg"
+                  src="/images/about/Endpoint-amico.svg"
                   alt="about-image"
                   fill
                   className="mx-auto hidden max-w-full drop-shadow-three dark:block dark:drop-shadow-none lg:mr-0"
